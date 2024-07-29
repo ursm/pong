ade08265c3384817df8e39e8024e5d18a19d44b9
Keita Urashima <ursm@ursm.jp>
2024-07-30 08:57:45 +0900

    Add test

---
+++ test/integration/smoke_test.rb
@@ -0,0 +1,27 @@
+require "test_helper"
+
+class SmokeTest < ActionDispatch::IntegrationTest
+  test "top" do
+    get "/"
+
+    assert_select "article", 4
+  end
+
+  test "year" do
+    get "/years/2024"
+
+    assert_select "article", 3
+  end
+
+  test "month" do
+    get "/years/2024/months/07"
+
+    assert_select "article", 2
+  end
+
+  test "day" do
+    get "/years/2024/months/07/days/01"
+
+    assert_select "article", 1
+  end
+end
