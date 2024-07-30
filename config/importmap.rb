5af210a7488b0b49b4ff92e2b83feec69cbade94
Keita Urashima <ursm@ursm.jp>
2024-07-30 16:02:05 +0900

    Do not load assets for admin

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,9 +1,13 @@
 pin "application"
 pin_all_from "app/javascript/controllers", under: "controllers"
 
-pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
+pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@rails/activestorage", to: "@rails--activestorage.js" # @7.1.3
-pin "stimulus-use" # @0.52.2
-pin "autosize" # @6.0.1
+
+pin "admin", preload: false
+pin_all_from "app/javascript/admin/controllers", under: "admin/controllers", preload: false
+
+pin "@rails/activestorage", to: "@rails--activestorage.js", preload: false # @7.1.3
+pin "autosize", preload: false # @6.0.1
+pin "stimulus-use", preload: false # @0.52.2
