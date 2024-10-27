f908712b6b1df5ea67b78f977e61ef77753e3b5c
Keita Urashima <ursm@ursm.jp>
2024-10-27 16:40:07 +0900

    8.0.0.rc1

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,12 +1,12 @@
 pin "application"
 pin_all_from "app/javascript/controllers", under: "controllers"
 
+pin "admin", preload: false
+pin_all_from "app/javascript/admin/controllers", under: "admin/controllers", preload: false
+
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
-
-pin "admin", preload: false
-pin_all_from "app/javascript/admin/controllers", under: "admin/controllers", preload: false
 
 pin "@rails/activestorage", to: "@rails--activestorage.js", preload: false # @7.1.3
 pin "autosize", preload: false # @6.0.1
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get "up",             to: "rails/health#show", as: :rails_health_check
+  get "up",             to: "rails/health#show",        as: :rails_health_check
   get "service-worker", to: "rails/pwa#service_worker", as: :pwa_service_worker
-  get "manifest",       to: "rails/pwa#manifest", as: :pwa_manifest
+  get "manifest",       to: "rails/pwa#manifest",       as: :pwa_manifest
 
   root "posts#index"
 
