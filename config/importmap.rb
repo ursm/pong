d533ff8820696c2d339262814ff2b01c00a389f3
Keita Urashima <ursm@ursm.jp>
2024-12-01 17:54:18 +0900

    importmap update

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,6 +8,6 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 
-pin "@rails/activestorage", to: "@rails--activestorage.js", preload: false # @7.1.3
+pin "@rails/activestorage", to: "@rails--activestorage.js", preload: false # @8.0.0
 pin "autosize", preload: false # @6.0.1
 pin "stimulus-use", preload: false # @0.52.2
