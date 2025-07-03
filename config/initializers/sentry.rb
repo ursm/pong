0b5162f466b6aef6dc852e90e1af1fbe42af8ac6
Keita Urashima <ursm@ursm.jp>
2025-07-03 22:19:56 +0900

    Filter healthcheck transaction

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -4,4 +4,8 @@ Sentry.init do |config|
   config.traces_sample_rate   = 1.0
   config.profiles_sample_rate = 1.0
   config.enable_logs          = true
+
+  config.before_send_transaction = ->(event, _hint) {
+    event.transaction == "Rails::HealthController#show" ? nil : event
+  }
 end
