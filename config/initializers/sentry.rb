95ea4751bf636da8c08bbcebf67aef680e930450
Keita <ursm@ursm.jp>
2026-09-02 08:29:44 +0900

    bundle update

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -3,7 +3,6 @@ Sentry.init do |config|
   config.send_default_pii     = true
   config.traces_sample_rate   = 1.0
   config.profiles_sample_rate = 1.0
-  config.enable_logs          = true
 
   config.before_send_transaction = ->(event, _hint) {
     event.transaction == 'Rails::HealthController#show' ? nil : event
